2781be47158a1573b7d3ee5ea7c8484828054e98
endone <84660082+Davaakhuu02@users.noreply.github.com>
2024-04-02 19:05:34 +0800

    start

---
+++ api/api/day1/lib/Models/todo.dart
@@ -0,0 +1,16 @@
+class Todo {
+  int id;
+  String title;
+  String date;
+  bool isDone;
+  String desc;
+  Todo (
+    {
+      required this.id,
+      required this.title,
+      required this.date,
+      required this.desc,
+      required this.isDone,
+    }
+  );
+}
